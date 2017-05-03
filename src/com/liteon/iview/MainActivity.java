be1f246254fef4818171039800d8b17018bab96c
Allen Sun <tel3342311@gmail.com>
2017-05-03 17:28:39 +0800

    Add exo player for streaming

--- src/com/liteon/iview/MainActivity.java
+++ src/com/liteon/iview/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent intent = new Intent();
+		intent.setClass(this, Records.class);
+		startActivity(intent);
 	}
 	
 	@Override
